6f32da27ab5032bdc19062a87fc3c0d99f0f989f
Prasad Ravan <prasadcravan@gmail.com>
2020-08-02 20:29:19 +0530

    Added client server initial code

---
+++ gRPC_Server_Streaming_Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gRPC_Server_Streaming_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
